3b1bac3d58786b0d423fc409ade07a9718a727eb
coding <coding@kims-MacBook-Air.local>
2019-02-19 19:48:04 +0800

    First commit

---
+++ src/components/Main.js
@@ -0,0 +1,25 @@
+require('normalize.css/normalize.css');
+require('styles/App.css');
+
+import React from 'react';
+
+
+let yeomanImage = require('../images/yeoman.png');
+
+class AppComponent extends React.Component {
+  render() {
+    return (
+
+      <div className="index">
+      
+        <img src={yeomanImage} alt="Yeoman Generator" />
+        <div className="notice">Please now  super fast<code>src/components/Main.js</code> to get started! Okosisi</div>
+      </div>
+    );
+  }
+}
+
+AppComponent.defaultProps = {
+};
+
+export default AppComponent;
